d1e288a2c526e5c4630e0d4410ba3837318dc798
Aakash Goel <aakash.lpin@gmail.com>
2016-02-07 21:29:21 +0530

    added request's body parser and connected with mongoose

--- src/server.js
+++ src/server.js
@@ -1,53 +1,26 @@
-/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
-
 import 'babel-core/polyfill';
-import path from 'path';
 import express from 'express';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import Router from './routes';
-import Html from './components/Html';
-
+import bodyParser from 'body-parser';
+// import multer from 'multer';
+import routes from './router/routes';
 const server = global.server = express();
-const port = process.env.PORT || 5000;
+// const upload = multer();
+const port = process.env.PORT || 5500;
 server.set('port', port);
 
-//
-// Register Node.js middleware
-// -----------------------------------------------------------------------------
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
-//
-// Register API middleware
-// -----------------------------------------------------------------------------
-server.use('/api/content', require('./api/content'));
+const dbPath = 'mongodb://localhost/streetsmart-development';
 
-//
-// Register server-side rendering middleware
-// -----------------------------------------------------------------------------
-server.get('*', async (req, res, next) => {
-  try {
-    let statusCode = 200;
-    const data = { title: '', description: '', css: '', body: '' };
-    const css = [];
-    const context = {
-      onInsertCss: value => css.push(value),
-      onSetTitle: value => data.title = value,
-      onSetMeta: (key, value) => data[key] = value,
-      onPageNotFound: () => statusCode = 404,
-    };
+import mongoose from 'mongoose';
+const db = mongoose.createConnection(dbPath);
 
-    await Router.dispatch({ path: req.path, context }, (state, component) => {
-      data.body = ReactDOM.renderToString(component);
-      data.css = css.join('');
-    });
-
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-    res.status(statusCode).send('<!doctype html>\n' + html);
-  } catch (err) {
-    next(err);
-  }
+db.on('error', () => {
+  throw new Error(`unable to connect to database at ${dbPath}`);
 });
+
+routes(server);
 
 //
 // Launch the server
